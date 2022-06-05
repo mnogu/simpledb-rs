daf4bebad5cd56322d4ce1bc624705da4fed1f9e
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-06-05 19:10:48 +0900

    Add txtest

--- src/tx.rs
+++ src/tx.rs
@@ -2,3 +2,4 @@ pub mod bufferlist;
 pub mod concurrency;
 pub mod recovery;
 pub mod transaction;
+pub mod txtest;
---
+++ src/tx/txtest.rs
@@ -0,0 +1,47 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::{file::blockid::BlockId, server::simpledb::SimpleDB, tx::transaction::Transaction};
+
+    #[test]
+    fn txtest() {
+        let mut db = SimpleDB::new("txtest", 400, 8).unwrap();
+        let lm = db.log_mgr();
+
+        let bm = db.buffer_mgr();
+
+        let mut tx1 = Transaction::new(lm.clone(), bm.clone()).unwrap();
+        let blk = BlockId::new("testfile", 1);
+        tx1.pin(&blk).unwrap();
+        tx1.set_int(&blk, 80, 1, false).unwrap();
+        tx1.set_string(&blk, 40, "one", false).unwrap();
+        tx1.commit().unwrap();
+
+        let mut tx2 = Transaction::new(lm.clone(), bm.clone()).unwrap();
+        tx2.pin(&blk).unwrap();
+        let ival = tx2.get_int(&blk, 80).unwrap();
+        let sval = tx2.get_string(&blk, 40).unwrap();
+        assert_eq!(1, ival);
+        assert_eq!("one", sval);
+        let newival = ival + 1;
+        let newsval = sval + "!";
+        tx2.set_int(&blk, 80, newival, true).unwrap();
+        tx2.set_string(&blk, 40, &newsval, true).unwrap();
+        tx2.commit().unwrap();
+        let mut tx3 = Transaction::new(lm.clone(), bm.clone()).unwrap();
+        tx3.pin(&blk).unwrap();
+        assert_eq!(2, tx3.get_int(&blk, 80).unwrap());
+        assert_eq!("one!", tx3.get_string(&blk, 40).unwrap());
+        tx3.set_int(&blk, 80, 9999, true).unwrap();
+        assert_eq!(9999, tx3.get_int(&blk, 80).unwrap());
+        tx3.rollback().unwrap();
+
+        let mut tx4 = Transaction::new(lm.clone(), bm.clone()).unwrap();
+        tx4.pin(&blk).unwrap();
+        assert_eq!(2, tx4.get_int(&blk, 80).unwrap());
+        tx4.commit().unwrap();
+
+        fs::remove_dir_all("txtest").unwrap();
+    }
+}
