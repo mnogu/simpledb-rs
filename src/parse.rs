81595ddf84057dd4e497d2be2494e6cc40ad886a
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-07-01 21:51:33 +0900

    Add lexertest

--- src/parse.rs
+++ src/parse.rs
@@ -1,2 +1,3 @@
 pub mod badsyntaxerror;
 pub mod lexer;
+pub mod lexertest;
---
+++ src/parse/lexertest.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod tests {
+    use crate::parse::lexer::Lexer;
+
+    #[test]
+    fn lexertest() {
+        let ss = vec![
+            "a=1", "1=a", "z=1", "1=z", "_=1", "1=_", "A=1", "1=A", "a=-1", "-1=a", " a = 1 ",
+            " 1 = a ",
+        ];
+        let e = vec![
+            ("a", 1),
+            ("a", 1),
+            ("z", 1),
+            ("z", 1),
+            ("_", 1),
+            ("_", 1),
+            ("a", 1),
+            ("a", 1),
+            ("a", -1),
+            ("a", -1),
+            ("a", 1),
+            ("a", 1),
+        ];
+        for (i, s) in ss.iter().enumerate() {
+            let mut lex = Lexer::new(&s);
+            let x;
+            let y;
+            if lex.match_id() {
+                x = lex.eat_id().unwrap();
+                lex.eat_delim('=').unwrap();
+                y = lex.eat_int_constant().unwrap();
+            } else {
+                y = lex.eat_int_constant().unwrap();
+                lex.eat_delim('=').unwrap();
+                x = lex.eat_id().unwrap();
+            }
+            assert_eq!(x, e[i].0);
+            assert_eq!(y, e[i].1);
+        }
+    }
+}
