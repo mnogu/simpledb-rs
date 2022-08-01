f5968275730a0785e7ab1793ce1fe6601307fb4f
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-08-02 08:09:03 +0900

    Add plannertest1

---
+++ src/plan/plannertest1.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+mod tests {
+    use std::{
+        fs,
+        sync::{Arc, Mutex},
+    };
+
+    use rand::{distributions::Uniform, prelude::Distribution};
+
+    use crate::{query::scan::ScanControl, server::simpledb::SimpleDB};
+
+    #[test]
+    fn plannertest1() {
+        let db = SimpleDB::new("plannertest1").unwrap();
+        let planner = db.planner().unwrap();
+        let tx = Arc::new(Mutex::new(db.new_tx().unwrap()));
+        let cmd = "create table T1(A int, B varchar(9))";
+        planner.execute_update(cmd, tx.clone()).unwrap();
+
+        let n = 200;
+        let mut rng = rand::thread_rng();
+        let die = Uniform::from(0..50);
+        for _ in 0..n {
+            let a = die.sample(&mut rng);
+            let cmd = format!("insert into T1(A,B) values({0}, 'rec{0}')", a);
+            planner.execute_update(&cmd, tx.clone()).unwrap();
+        }
+
+        let qry = "select B from T1 where A=10";
+        let p = planner.create_query_plan(qry, tx.clone()).unwrap();
+        let mut s = p.open().unwrap();
+        while s.next().unwrap() {
+            assert_eq!(s.get_string("b").unwrap(), "rec10");
+        }
+        s.close().unwrap();
+        tx.lock().unwrap().commit().unwrap();
+
+        fs::remove_dir_all("plannertest1").unwrap();
+    }
+}
