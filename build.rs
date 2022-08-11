17d578bc23d685ab08747a46b9fff6a3ec518614
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-08-11 23:27:46 +0900

    Remove an extra line

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::{env, path::PathBuf};
 
 fn main() {
-    println!("=================");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("simpledb_descriptor.bin"))
