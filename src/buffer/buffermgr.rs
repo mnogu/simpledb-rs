e01461531176e15c79ce2d2df855d4904e797b8e
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-08-28 22:15:08 +0900

    Use is_none()

--- src/buffer/buffermgr.rs
+++ src/buffer/buffermgr.rs
@@ -100,7 +100,7 @@ impl BufferMgr {
 
     fn try_to_pin(&mut self, blk: &BlockId) -> Result<Option<usize>, Error> {
         let mut idx = self.find_existing_buffer(blk);
-        if let None = idx {
+        if idx.is_none() {
             idx = self.choose_unpinned_buffer();
             if let Some(i) = idx {
                 self.bufferpool[i].assign_to_block(blk.clone())?;
