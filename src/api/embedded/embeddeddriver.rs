56edcf2d7f7fca95ed554ca8e531417d95df8a29
Muneyuki Noguchi <nogu.dev@gmail.com>
2022-08-30 09:46:57 +0900

    Ignore URL prefixes with ":"

--- src/api/embedded/embeddeddriver.rs
+++ src/api/embedded/embeddeddriver.rs
@@ -20,7 +20,10 @@ impl EmbeddedDriver {
 
 impl DriverControl for EmbeddedDriver {
     fn connect(&self, url: &str) -> Result<Connection, SQLError> {
-        let dbname = url;
+        let mut dbname = url;
+        if let Some(idx) = dbname.rfind(':') {
+            dbname = &dbname[idx + 1..]
+        }
         let db = SimpleDB::new(dbname)?;
         Ok(Arc::new(Mutex::new(EmbeddedConnection::new(db)?)).into())
     }
